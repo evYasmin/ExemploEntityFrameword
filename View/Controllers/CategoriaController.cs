1aef88ca498ac3757ea0233c05bc92c45e943514
evYasmin <49412927+evYasmin@users.noreply.github.com>
2019-07-31 17:30:40 -0300

    Projeto iniciado

---
+++ View/Controllers/CategoriaController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace View.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// Método que permitirá ter acesso aos dados das categorias,
+        /// filtrando, ordenando e paginando as informações de acirdi 
+        /// com os parametros
+        /// </summary>
+        /// <param name="buscar"></param>
+        /// <param name="quantidade"></param>
+        /// <param name="pagina"></param>
+        /// <param name="colunaOrdem"></param>
+        /// <param name="ordem"></param>
+        /// <returns></returns>
+        [HttpGet, Route("obtertodos")]
+        public IActionResult ObterTodos(
+            string buscar = "", int quantidade = 10, int pagina = 0,
+            string colunaOrdem = "nome", string ordem = "ASC")
+        {
+            return Ok();
+        }
+    }
+}
